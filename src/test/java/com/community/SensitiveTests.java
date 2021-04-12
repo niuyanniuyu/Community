da8d83775edfad472db525eb6c28879044fd2cba
root <1009743898@qq.com>
2021-04-12 14:42:35 +0800

    2021-04-12 14:42

---
+++ src/test/java/com/community/SensitiveTests.java
@@ -0,0 +1,24 @@
+package com.community;
+
+import com.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博，可以嫖娼，可以开票,不可以吸冰粉";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
